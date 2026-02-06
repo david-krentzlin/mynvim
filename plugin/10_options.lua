1153530cb9f194c28620e1653327dd78dd98ec18
David Krentzlin <david.krentzlin@new-work.se>
2026-02-06 14:26:41 +0100

    only show warnings and up for now in diagnostics

--- plugin/10_options.lua
+++ plugin/10_options.lua
@@ -116,13 +116,13 @@ local diagnostic_opts = {
   },
 
   -- Show all diagnostics as underline (for their messages type `<Leader>ld`)
-  underline = { severity = { min = 'HINT', max = 'ERROR' } },
+  underline = { severity = { min = 'WARN', max = 'ERROR' } },
 
   -- Show more details immediately for errors on the current line
   virtual_lines = false,
   virtual_text = {
     current_line = true,
-    severity = { min = 'ERROR', max = 'ERROR' },
+    severity = { min = 'WARN', max = 'ERROR' },
   },
 
   -- Don't update diagnostics when typing
